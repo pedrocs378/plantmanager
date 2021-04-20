74c525c4b4bddb0518af9becfd0fe35058be416a
Pedro César <pedrocs378@gmail.com>
2021-04-20 11:11:59 -0300

    chore: adiciona modo para usuario sair do teclado ao tocar fora

--- src/pages/UserIdentification/index.tsx
+++ src/pages/UserIdentification/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigation } from '@react-navigation/core'
-import { SafeAreaView, Text, TextInput, View, KeyboardAvoidingView, Platform } from 'react-native'
+import { SafeAreaView, Text, TextInput, View, KeyboardAvoidingView, TouchableWithoutFeedback, Platform, Keyboard } from 'react-native'
 
 import { Button } from '../../components/Button'
 import colors from '../../styles/colors'
@@ -34,34 +34,36 @@ export function UserIdentification() {
 				style={styles.container}
 				behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
 			>
-				<View style={styles.content}>
-					<View style={styles.form}>
-						<View style={styles.header}>
-							<Text style={styles.emoji}>
-								{isFilled ? '😄' : '😃'}
-							</Text>
-							<Text style={styles.title}>
-								Como podemos {'\n'}
-								chamar você?
-							</Text>
-						</View>
+				<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+					<View style={styles.content}>
+						<View style={styles.form}>
+							<View style={styles.header}>
+								<Text style={styles.emoji}>
+									{isFilled ? '😄' : '😃'}
+								</Text>
+								<Text style={styles.title}>
+									Como podemos {'\n'}
+									chamar você?
+								</Text>
+							</View>
 
-						<TextInput
-							style={[
-								styles.input,
-								(isFocused || isFilled) && { borderColor: colors.green }
-							]}
-							placeholder="Digite um nome"
-							onBlur={handleInputBlur}
-							onFocus={() => setIsFocused(true)}
-							onChangeText={handleInputChange}
-						/>
+							<TextInput
+								style={[
+									styles.input,
+									(isFocused || isFilled) && { borderColor: colors.green }
+								]}
+								placeholder="Digite um nome"
+								onBlur={handleInputBlur}
+								onFocus={() => setIsFocused(true)}
+								onChangeText={handleInputChange}
+							/>
 
-						<View style={styles.footer}>
-							<Button onPress={handleSubmit}>Confirmar</Button>
+							<View style={styles.footer}>
+								<Button onPress={handleSubmit}>Confirmar</Button>
+							</View>
 						</View>
 					</View>
-				</View>
+				</TouchableWithoutFeedback>
 			</KeyboardAvoidingView>
 		</SafeAreaView>
 	)
